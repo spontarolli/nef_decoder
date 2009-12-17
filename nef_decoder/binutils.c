7b51d0b65d98a18e09f35cffb329e80cadec605f
fpierfed <fpierfed@ef6302ea-6b07-0410-9385-fd2030aa736e>
2009-12-17 04:11:25 +0000

    implemented support for negative numbers and cleaned up the API.

--- nef_decoder/binutils.c
+++ nef_decoder/binutils.c
@@ -8,12 +8,18 @@ Credits: c_bin2dec dilip.mathews: http://www.daniweb.com/code/snippet216372.html
 
 
 const char* inttobin(int);
+void strtobin(const char*, int, char*);
 int bintoint(const char *, int);
 
 
-const char* inttobin(int num) {
+
+const char* positive_inttobin32(int num) {
+    // Convert `num` into its binary representation.
     static char bin[32 + 1] = { 0 };
     int i = 0;
+    
+    // Make sure bin is terminated.
+    bin[32] = '\0';
     
     memset(bin, '0', 32);
     while (num > 0) {
@@ -24,7 +30,143 @@ const char* inttobin(int num) {
 }
 
 
+
+const char* negative_inttobin32(int num) {
+    // Convert `num` into its binary representation.
+    static char bin[32 + 1] = { 0 };
+    int i = 0;
+    
+    // Make sure bin is terminated.
+    bin[32] = '\0';
+    
+    memset(bin, '1', 32);
+    
+    // flip the sign of num to turn it into a positive integer and subtract one.
+    // the rest is identical to positive_inttobin32 only with 0s and 1s flipped.
+    num = (-num) - 1;
+    while (num > 0) {
+        bin[32 - 1 - i++] = '1' - (num % 2);
+        num >>= 1;
+    }
+    return bin;
+}
+
+
+
+const char* inttobin32(int num) {
+    if(num == 0)
+        return("00000000000000000000000000000000\0");
+    else if(num > 0)
+        return(positive_inttobin32(num));
+    else
+        return(negative_inttobin32(num));
+}
+
+
+
+const char* positive_inttobin8(int num) {
+    // Convert `num` into its binary representation.
+    static char bin[8 + 1] = { 0 };
+    int i = 0;
+    
+    // Make sure bin is terminated.
+    bin[8] = '\0';
+    
+    memset(bin, '0', 8);
+    while (num > 0) {
+        bin[8 - 1 - i++] = '0' + num % 2;
+        num >>= 1;
+    }
+    return bin;
+}
+
+
+
+const char* negative_inttobin8(int num) {
+    // Convert `num` into its binary representation.
+    static char bin[8 + 1] = { 0 };
+    int i = 0;
+    
+    // Make sure bin is terminated.
+    bin[8] = '\0';
+    
+    memset(bin, '1', 8);
+    
+    // flip the sign of num to turn it into a positive integer and subtract one.
+    // the rest is identical to positive_inttobin8 only with 0s and 1s flipped.
+    num = (-num) - 1;
+    while (num > 0) {
+        bin[8 - 1 - i++] = '1' - (num % 2);
+        num >>= 1;
+    }
+    return bin;
+}
+
+
+
+const char* inttobin8(int num) {
+    if(num == 0)
+        return("00000000\0");
+    else if(num > 0)
+        return(positive_inttobin8(num));
+    else
+        return(negative_inttobin8(num));
+}
+
+
+
+void positive_inttobin_var(int num, int count, char* bin) {
+    // Convert `num` into its binary representation.
+    // bin is already allocated and of length count.
+    int i = 0;
+    
+    // Make sure that bin is null terminated.
+    bin[count] = '\0';
+    
+    memset(bin, '0', count);
+    while (num > 0) {
+        bin[count - 1 - i++] = '0' + num % 2;
+        num >>= 1;
+    }
+    return;
+}
+
+
+
+void negative_inttobin_var(int num, int count, char* bin) {
+    // Convert `num` into its binary representation.
+    // bin is already allocated and of length count.
+    int i = 0;
+    
+    // Make sure that bin is null terminated.
+    bin[count] = '\0';
+    
+    memset(bin, '1', count);
+    
+    // flip the sign of num to turn it into a positive integer and subtract one.
+    // the rest is identical to positive_inttobin32 only with 0s and 1s flipped.
+    num = (-num) -1;
+    while (num > 0) {
+        bin[count - 1 - i++] = '1' - num % 2;
+        num >>= 1;
+    }
+    return;
+}
+
+
+
+void inttobin_var(int num, int count, char* bin) {
+    // Convert `num` into its binary representation.
+    // bin is already allocated and of length count.
+    if(num >= 0)
+        return(positive_inttobin_var(num, count, bin));
+    return(negative_inttobin_var(num, count, bin));
+}
+
+
+
 int bintoint(const char *bin, int len) {
+    // Convert the binary string `bin` of length `len` into an integer.
     int  b , k, n;
     int  sum = 0; 
  
@@ -41,27 +183,94 @@ int bintoint(const char *bin, int len) {
 
 
 
+void strtobin(const char* data, const int len, char* bin) {
+    // Convert the binary data `data` (coming from a file, presumably) into a 
+    // string of 1s and 0s. 8 bits per `data` character.
+    // We assume that bin is already allocated and is of size len*8+1
+    long i;
+    long j;
+    char temp[8];
+    
+        
+    // Make sure bin is nicely terminated.
+    bin[len*8] = '\0';
+    
+    for(j=i=0; i<len; i++) {
+        inttobin_var((unsigned)data[i], 8, temp);
+        
+        bin[j++] = temp[0];
+        bin[j++] = temp[1];
+        bin[j++] = temp[2];
+        bin[j++] = temp[3];
+        bin[j++] = temp[4];
+        bin[j++] = temp[5];
+        bin[j++] = temp[6];
+        bin[j++] = temp[7];
+    }
+    return;
+}
+
+
+
 PyDoc_STRVAR(doc_int2bin, 
-        "int-> (ascii). Encode a positive integer into a string of 1s and 0s.");
+        "int-> (ascii). Encode an integer into a string of 1s and 0s.");
 static PyObject * int2bin(PyObject *self, PyObject *args) {
     int n;
     const int count;
+    char* bin=NULL;
 
     
     // Parse input arguments.
     if (!PyArg_ParseTuple(args, "ii", &n, &count))
         return(NULL);
     
-    // If we get a negative integer, rise an exception. Otherwise convert the
-    // integer.
-    // FIXME: handle the case n < 0
-    if(n < 0) {
-        PyErr_SetString(PyExc_TypeError, 
-                        "negative integers are not supported.\n");
+    // Use specialized functions if possible.
+    switch(count) {
+        case 32:
+            return(Py_BuildValue("s", inttobin32(n)));
+        case 8:
+            return(Py_BuildValue("s", inttobin8(n)));
+        default:
+            // Allocate memory for the return string.
+            bin = (char*)malloc((count+1) * sizeof(char));
+            if(bin == NULL) {
+                PyErr_SetString(PyExc_MemoryError, "Memory allocation error.\n");
+                return(NULL);
+            }
+            inttobin_var(n, count, bin);
+            return(Py_BuildValue("s", bin));
+    }
+}
+
+
+
+PyDoc_STRVAR(doc_data2bin, 
+        "data-> (ascii). Encode a stream of bytes into a string of 1s and 0s.");
+static PyObject * data2bin(PyObject *self, PyObject *args) {
+    char* data;
+    const int len;
+    char* bin=NULL;
+    
+    
+    // Parse input arguments.
+    if (!PyArg_ParseTuple(args, "s#", &data, &len))
+        return(NULL);
+    
+    if(len <= 0)
+        Py_RETURN_NONE;
+    
+    // Allocate memory for the return string.
+    bin = (char*)malloc((len*8 + 1) * sizeof(char));
+    if(bin == NULL) {
+        PyErr_SetString(PyExc_MemoryError, "Memory allocation error.\n");
         return(NULL);
     }
-    return(Py_BuildValue("s", inttobin(n)));
+    
+    strtobin(data, len, bin);
+    return(Py_BuildValue("s", bin));
 }
+
+
 
 
 PyDoc_STRVAR(doc_bin2int, 
@@ -108,9 +317,11 @@ static PyObject * bin2int(PyObject *self, PyObject *args, PyObject *keywds) {
 
 static PyMethodDef binutils_methods[] = {
     {"int2bin",  int2bin, METH_VARARGS, doc_int2bin},
+    {"data2bin",  data2bin, METH_VARARGS, doc_data2bin},
     {"bin2int",  (PyCFunction)bin2int, METH_VARARGS|METH_KEYWORDS, doc_bin2int},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
+
 
 
 PyMODINIT_FUNC initbinutils(void)
--- nef_decoder/test_binutils.py
+++ nef_decoder/test_binutils.py
@@ -1,6 +1,9 @@
+import struct
+import sys
 import time
 
 import binutils
+
 
 
 
@@ -12,21 +15,69 @@ def int2bin(n, count=36):
 
 
 dt = 0
-for i in range(N):
+for i in range(-N, N, 1):
     t0 = time.time()
-    bin_str = binutils.int2bin(i, 36)
+    bin_str = binutils.int2bin(i, 32)
     dt += time.time() - t0
     
-    assert(binutils.bin2int(bin_str) == i)
-print('binutils: %d iterations: %.02fs' % (N, dt))
-print('binutils: %.02f iterations/s' % (float(N) / dt))
+    if(i >= 0):
+        assert(binutils.bin2int(bin_str) == i)
+    else:
+        assert(int(bin_str, 2) - 2**32 == i)
+print('binutils: %d int -> bin: %.02fs' % (2*N, dt))
+print('binutils: %.02f int -> bin/s' % (float(2*N) / dt))
 
 
-dt = 0
-for i in range(N):
+# dt = 0
+# for i in range(-N, N, 1):
+#     t0 = time.time()
+#     bin_str = int2bin(i, 36)
+#     dt += time.time() - t0
+#     assert(int(bin_str, 2) == i)
+# print('slow: %d iterations: %.02fs' % (N, dt))
+# print('slow: %.02f iterations/s' % (float(N) / dt))
+
+
+test_data2bin = True
+try:
+    data = open(sys.argv[1], 'rb').read()
+except:
+    print('Specify an input file to test data2bin.')
+    test_data2bin = False
+if(test_data2bin):
+    barray = ''.join([binutils.int2bin(i, 32)[24:] 
+                      for i in struct.unpack('%dB' % (len(data)), data)])
+    
     t0 = time.time()
-    bin_str = int2bin(i, 36)
-    dt += time.time() - t0
-    assert(int(bin_str, 2) == i)
-print('slow: %d iterations: %.02fs' % (N, dt))
-print('slow: %.02f iterations/s' % (float(N) / dt))
+    mine = binutils.data2bin(data)
+    dt = time.time() - t0
+    print('binutils: str[%d] -> bin: %.02fs' % (len(data), dt))
+    print('binutils: %.02f ch -> bin/s' % (float(len(data)) / dt))
+    assert(len(mine) == 8 * len(data))
+    assert(mine == barray)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
