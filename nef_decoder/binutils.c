be8f39359cf6d92aad53ab34aa282ec8e3f23ab5
fpierfed <fpierfed@ef6302ea-6b07-0410-9385-fd2030aa736e>
2009-12-17 01:04:29 +0000

    added comments and other niceties.

--- nef_decoder/binutils.c
+++ nef_decoder/binutils.c
@@ -1,4 +1,10 @@
+/*
+                        binary <-> decimal utilities
+
+Credits: c_bin2dec dilip.mathews: http://www.daniweb.com/code/snippet216372.html
+*/
 #include <Python.h>
+
 
 
 const char* inttobin(int);
@@ -26,10 +32,6 @@ int bintoint(const char *bin, int len) {
     for(k = 0; k <= len; k++) {
         b = 1;
         n = (bin[k] - '0'); // char to numeric value
-        if ((n > 1) || (n < 0)) {
-            puts("\n\n ERROR! BINARY has only 1 and 0!\n");
-            return (0);
-        }
         b = b<<(len-k);
         // sum it up
         sum = sum + n * b;
@@ -39,43 +41,74 @@ int bintoint(const char *bin, int len) {
 
 
 
-
+PyDoc_STRVAR(doc_int2bin, 
+        "int-> (ascii). Encode a positive integer into a string of 1s and 0s.");
 static PyObject * int2bin(PyObject *self, PyObject *args) {
     int n;
     const int count;
 
     
+    // Parse input arguments.
     if (!PyArg_ParseTuple(args, "ii", &n, &count))
         return(NULL);
     
-    if(n < 0)
+    // If we get a negative integer, rise an exception. Otherwise convert the
+    // integer.
+    // FIXME: handle the case n < 0
+    if(n < 0) {
+        PyErr_SetString(PyExc_TypeError, 
+                        "negative integers are not supported.\n");
         return(NULL);
-    else if(n == 0)
-        return(Py_BuildValue("s", "0"));
-    
+    }
     return(Py_BuildValue("s", inttobin(n)));
 }
 
 
-static PyObject * bin2int(PyObject *self, PyObject *args) {
-    const char *s;
+PyDoc_STRVAR(doc_bin2int, 
+        "(ascii) -> int. Decode a string of 1s and 0s into an integer.");
+static PyObject * bin2int(PyObject *self, PyObject *args, PyObject *keywds) {
+    const char* s;
+    PyObject* unsafe=Py_False;
     const int len;
+    int i;
 
     
-    if (!PyArg_ParseTuple(args, "s#", &s, &len))
+    // These are the input keywords:
+    static char *kwlist[] = {"bin_str", "unsafe", NULL};
+    
+    // Parse the input string.
+    if (!PyArg_ParseTupleAndKeywords(args, 
+                                     keywds, 
+                                     "s#|O", 
+                                     kwlist, 
+                                     &s, 
+                                     &len, 
+                                     &unsafe))
         return(NULL);
     
+    // Make sure that we have a string of size > 0, otherwise return None.
     if(len <= 0)
-        return(NULL);
+        Py_RETURN_NONE;
     
+    // Simple sanity check: make sure that s contains only 0s and 1s.
+    if(PyObject_Not(unsafe) != 0) {
+        for(i=0; i<len; i++) {
+            if(s[i] != '0' && s[i] != '1') {
+                PyErr_SetString(PyExc_TypeError, "binary strings only have 1 and 0\n");
+                return(NULL);
+            }
+        }
+    }
+    
+    // Convert the binary string into an integer and return it as Python int.
     return(Py_BuildValue("i", bintoint(s, len)));
 }
 
 
 
 static PyMethodDef binutils_methods[] = {
-    {"int2bin",  int2bin, METH_VARARGS, "Convert a positive interger into its binary representation."},
-    {"bin2int",  bin2int, METH_VARARGS, "Convert a binary string into an integer."},
+    {"int2bin",  int2bin, METH_VARARGS, doc_int2bin},
+    {"bin2int",  (PyCFunction)bin2int, METH_VARARGS|METH_KEYWORDS, doc_bin2int},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
