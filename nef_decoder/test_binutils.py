2aef4a0f2ded87d30a3b4349689430ee2e7d734b
fpierfed <fpierfed@ef6302ea-6b07-0410-9385-fd2030aa736e>
2009-12-17 01:04:09 +0000

    made it self consistent.

--- nef_decoder/test_binutils.py
+++ nef_decoder/test_binutils.py
@@ -1,10 +1,14 @@
 import time
 
 import binutils
-import slow
+
 
 
 N = int(1e5)
+def int2bin(n, count=36):
+    """returns the binary of integer n, using count number of digits"""
+    return "".join([str((n >> y) & 1) for y in range(count-1, -1, -1)])
+
 
 
 dt = 0
@@ -21,7 +25,7 @@ print('binutils: %.02f iterations/s' % (float(N) / dt))
 dt = 0
 for i in range(N):
     t0 = time.time()
-    bin_str = slow.int2bin(i, 36)
+    bin_str = int2bin(i, 36)
     dt += time.time() - t0
     assert(int(bin_str, 2) == i)
 print('slow: %d iterations: %.02fs' % (N, dt))
